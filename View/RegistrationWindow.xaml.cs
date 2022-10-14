d9787580353c3e77eb0891ad154c7c302bcfccdc
ilyaschevchenko <ilyaschevchenko@ILYASCHEVCH3FA7>
2022-10-14 14:48:07 +0300

    update reg window

--- View/RegistrationWindow.xaml.cs
+++ View/RegistrationWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace DetailingV12App.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
